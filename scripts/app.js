89697d7c23232cd6f248297eb63b8a8cbe42b420
James Thomas <jt.2012@yahoo.com>
2017-03-16 12:11:43 -0700

    Worked on template functionality

--- scripts/app.js
+++ scripts/app.js
@@ -8,8 +8,30 @@ function Project(projectData){
   this.dateStarted = projectData.dateStarted;
   this.dateCompleted = projectData.dateCompleted;
   this.description = projectData.description;
+  this.imgUrl = projectData.imgUrl;
 }
+
+Project.prototype.calculateTime = function(){
+  var time = this.dateStarted + this.dateCompleted;
+  return time;
+};
 
 allProjectsData.forEach(function(projectObject){
   projects.push(new Project(projectObject));
+});
+
+Project.prototype.toHtml = function(){
+  var $newProject = $('div.template').clone().removeClass('template');
+
+  $newProject.find('.info h6').text(this.title);
+  $newProject.find('.time').text(this.time);
+  $newProject.find('.description').text(this.description);
+  $newProject.find('.info a').attr('href', this.projectUrl);
+  $newProject.find('.template img').attr('src', this.imgUrl);
+
+  return $newProject;
+};
+
+projects.forEach(function(a){
+  $('#project-overview').append(a.toHtml());
 });
--- scripts/projectData.js
+++ scripts/projectData.js
@@ -6,19 +6,22 @@ var allProjectsData = [
     projectUrl: 'https://ithalis.github.io/About-Me/',
     dateStarted: '06 Feb 2017',
     dateCompleted: '12 Feb 2017',
-    description: 'My very first JavaScript project! Worked with for loops and if/else statements to build a groundwork for vanilla JavaScript.'
+    description: 'My very first JavaScript project! Worked with for loops and if/else statements to build a groundwork for vanilla JavaScript.',
+    imgUrl: ''
   },
   {
     title: 'Salmon Cookies',
     projectUrl: 'https://ithalis.github.io/salmon-cookies/',
     dateStarted: '13 Feb 2017',
     dateCompleted: '19 Feb 2017',
-    description: 'My first attempt at using functions and forms. The most difficult thing was learning how to write to the DOM.'
+    description: 'My first attempt at using functions and forms. The most difficult thing was learning how to write to the DOM.',
+    imgUrl: ''
   },
   {
     title: 'Fisticuffs',
     projectUrl: 'https://endere.github.io/Histicuffs/',
     dateStarted: '27 Feb 2017',
     dateCompleted: '03 Mar 2017',
-    description: 'My biggest project yet. Completed with a group of 3 others. Cemented what I had learned over the month before and learned a lot about coding with others.'
+    description: 'My biggest project yet. Completed with a group of 3 others. Cemented what I had learned over the month before and learned a lot about coding with others.',
+    imgUrl: 'imgs/Fisticuffs.jpg'
   }];
